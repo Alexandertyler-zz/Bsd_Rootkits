63de9f60f1b4cdebf543ca7557b37a25209c1e11
Alex Tyler <at@norsecorp.com>
2015-09-13 15:10:53 -0700

    Finished the cd_exmaple c file but no debugging yet

--- cd_example/cd_example.c
+++ cd_example/cd_example.c
@@ -24,4 +24,74 @@ static struct cdevsw cd_example_cdevsw =
 	.d_name = 	"cd_example";
 };
 
+static char buf[512 + 1];
+static size_t len;
 
+int
+open(struct cdev *dev, int flag, int otype, struct thread *td)
+{
+	memset(&buf, '\0', 513);
+	len = 0;
+
+	return(0);
+}
+
+int
+close(struct cdev *dev, int flag, int otype, struct thread *td)
+{
+	return(0);
+}
+
+int
+write(struct cdev *dev, struct uio *uio, int ioflag)
+{
+	int error = 0;
+
+	error = copyinstr(uio->uio_iov->iov_base, *buf, 512, &len);
+	if (error !=0)
+		uprintf("Write to \"cd_example\" failed.\n");
+
+	return(error);
+}
+
+int
+read(struct cdev *dev, struct uio *uio, int ioflag))
+{
+	int error = 0;
+
+	if (len <= 0)
+		error = -1;
+	else
+		copystr(&buf, uio->uio_iov->iov_base, 512, &len);
+
+	return(error);
+}
+
+static struct cdev *sdev;
+
+static int
+load(struct module *module, int cmd, void *args)
+{
+	int error = 0;
+
+	switch (cmd)
+	{
+	case MOD_LOAD:
+		sdev = make_dev(&cd_example_cdevsw, 0, UID_ROOT, GID_WHEEL, 0600, 
+		    "cd_example");
+		uprintf("Character device loaded.\n");
+		break;
+	
+	case MOD_UNLOAD:
+		destroy_dev(sdev);
+		uprintf("Character device unloaded.\n");
+		break;
+
+	default:
+		error = EOPNOTSUPP;
+		break;
+	}
+
+	return(error);
+}
+DEV_MODULE(cd_example, load, NULL);
